9fb907f85fd0d785f2852414fcf3c9e6bef4a14d
Krigovszki Bálint <krigovszki.balint@gmail.com>
2021-11-30 18:17:13 +0100

    UI: Register Page

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,20 @@
+import "./register.css"
+
+export default function Login() {
+    return (
+        <div className="register">
+            <span className="registerTitle">Register</span>
+            <form className="registerForm">
+                <label>Username</label>
+                <input className="registerInput" type="text" placeholder="Username" />
+                <label>E-mail</label>
+                <input className="registerInput" type="text" placeholder="E-mail" />
+                <label>Password</label>
+                <input className="registerInput" type="password" placeholder="Password" />
+                <button className="registerButton">Register</button>
+            </form>
+            <hr />
+            <button className="registerLoginButton">Login</button>
+        </div>
+    )
+}
