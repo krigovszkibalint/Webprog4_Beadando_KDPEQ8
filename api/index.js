0c80155814076f601c73bc0805a50ccde74c75b1
Krigovszki Bálint <krigovszki.balint@gmail.com>
2021-12-02 04:15:26 +0100

    User Update

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 app.use(express.json());
@@ -13,6 +14,8 @@ mongoose
 .catch(err=>console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
 
 app.listen("3000", () =>{
     console.log("Backend running");
