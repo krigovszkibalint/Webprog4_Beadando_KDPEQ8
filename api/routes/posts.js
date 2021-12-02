9f50a47ec2623bab3f95093bf429e88417e2478c
Krigovszki Bálint <krigovszki.balint@gmail.com>
2021-12-02 23:49:07 +0100

    Request Query

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -65,6 +65,25 @@ router.get("/:id", async (req,res) =>{
     }
 });
 
-
+//GET ALL POSTS
+router.get("/", async (req,res) =>{
+    const username = req.query.user;
+    const category = req.query.category;
+    try {
+        let posts;
+        if (username) {
+            posts = await Post.find({username})
+        } else if (category) {
+            posts = await Post.find({categories:{
+                $in:[category]
+            }})
+        } else {
+            posts = await Post.find();
+        }
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
