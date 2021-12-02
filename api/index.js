8cb0646d2aa83ac59b9f37451fd7e230731ed0f3
Krigovszki Bálint <krigovszki.balint@gmail.com>
2021-12-02 03:00:29 +0100

    Register Route

--- api/index.js
+++ api/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
 .connect(process.env.MONGO_URL)
 .then(console.log("Connected"))
 .catch(err=>console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("3000", () =>{
     console.log("Backend running");
